fd48a5446fb818f99851ee15b817d1c3c078aed8
Remi Gau <remi_gau@hotmail.com>
2020-09-09 12:19:12 +0200

    fix style

--- check_my_code.m
+++ check_my_code.m
@@ -1,60 +1,73 @@
-function [error_code, file_function, cplx, percentage_comment] = check_my_code(RECURSIVE, CPLX_THRS, COMMENT_THRS, PRINT_FILE)
-    % This will give you the The McCabe complexity of all the `.m` files in the current directory.
-    % It will also return the complexity of the subfunctions in each file. If it gets above 10
-    % you enter the danger zone. If you are above 15 you might seriously reconsider refactoring
-    % those functions.
+function varargout = check_my_code(RECURSIVE, CPLX_THRS, COMMENT_THRS, PRINT_FILE)
+    % [error_code, file_function, cplx, percentage_comment] = check_my_code(...
+    %                                                              RECURSIVE, ...
+    %                                                              CPLX_THRS, ...
+    %                                                              COMMENT_THRS, ...
+    %                                                              PRINT_FILE)
     %
-    % This function  also checks the proportion of lines with comments in each file (might overestimate it).
+    % This will give you the The McCabe complexity of all the `.m` files in the current
+    %  directory.
+    % It will also return the complexity of the subfunctions in each file. If it gets above 10
+    %  you enter the danger zone. If you are above 15 you might seriously reconsider refactoring
+    %  those functions.
+    %
+    % This function  also checks the proportion of lines with comments in each file
+    %  (might overestimate it).
     % In general you might want to try to be around 20%.
     %
-    % This function will then list the functions that do not meet the requirements you have set for your projects.
+    % This function will then list the functions that do not meet the requirements you have
+    % set for your projects.
     % You can then use this information to figure out which function you should refactor first.
     %
-    % FYI: The McCabe complexity of a function is presents how many paths one can take while navigating through
-    % the conditional statements of your function (`if`, `switch`, ...).
+    % FYI: The McCabe complexity of a function is presents how many paths one can take while
+    % navigating through the conditional statements of your function (`if`, `switch`, ...).
     %
     % ## USAGE
-    % If the check_my_code function is in the matlab path, then simply calling it will check the files in the
-    % the current directory.
+    % If the check_my_code function is in the matlab path, then simply calling it will check
+    % the files in the the current directory.
     %
     % ### INPUTS
-    % #### RECURSIVE : BOOLEAN if set to true this will check the .m files in all the subfolders. (default: false)
+    % #### RECURSIVE : BOOLEAN if set to true this will check the .m files in all the
+    %     subfolders. (default: false)
     %
-    % #### CPLX_THRS : 1 X 2 ARRAY : thresholds for the acceptable McCabe complexity before triggering a warning.
-    %     Having 2 values lets you decide a zone of complexity that is high but acceptable and another that is
-    %     too high. (default: [15 20])
+    % #### CPLX_THRS : 1 X 2 ARRAY : thresholds for the acceptable McCabe complexity before
+    %     triggering a warning.
+    %     Having 2 values lets you decide a zone of complexity that is high but acceptable and
+    %     another that is too high. (default: [15 20])
     %
-    % #### COMMENT_THRS : 1 X 2 ARRAY : thresholds for the acceptable percentage of comments in a file
-    %     before triggering a warning.
-    %     Having 2 values lets you decide levels that are low but acceptable and another that is
-    %     too low. (default: [20 10])
+    % #### COMMENT_THRS : 1 X 2 ARRAY : thresholds for the acceptable percentage of comments
+    %     in a file before triggering a warning.
+    %     Having 2 values lets you decide levels that are low but acceptable and another that
+    %     is too low. (default: [20 10])
     %
-    % #### PRINT_FILE : BOOLEAN this will print a file with the overall error code ; mostly used for automation
-    %     for now. (default: true)
+    % #### PRINT_FILE : BOOLEAN this will print a file with the overall error code ; mostly
+    %     used for automation for now. (default: true)
+    %
     %
     % ### OUPUTS
     %
     % #### error_code
-    % an array wth [cplx_error_code comment_error_code] where each value is 0 if there is no file that
-    % is too complex or has too few comments and is 1 otherwise
+    %    an array wth [cplx_error_code comment_error_code] where each value is 0 if there is no
+    %    file that is too complex or has too few comments and is 1 otherwise
     %
     % #### file_function
-    % a n X 2 cell listing of all the function in {i,1} and subfunction in {i,2} tested. If the
-    % function is the main function of a file then {i,1} and {i,2} are the same.
+    %    A n X 2 cell listing of all the function in {i,1} and subfunction in {i,2} tested.
+    %    If the function is the main function of a file then {i,1} and {i,2} are the same.
     %
     % #### cplx
-    % an array with the complexity of each function and subfunction
+    %    An array with the complexity of each function and subfunction
     %
     % #### percentage_comment
-    % an array with the percentage of comment in each file
+    %    An array with the percentage of comment in each file
+    %
     %
     % ## IMPLEMENTATION
     %
-    % It relies on the linter used natively by matlab so it could also be extended to check all the
-    % messages relating to
-    % all the little other issues in your code that you have not told matlab to ignore.
+    % It relies on the linter used natively by matlab so it could also be extended to check
+    %  all the messages relating to all the little other issues in your code that you have not
+    %  told matlab to ignore.
     %
-    % Because octave does not have a linter, so this will only work with matlab.
+    % Because octave does not have a linter, this will only work with matlab.
 
     clc;
 
@@ -89,7 +102,7 @@ function [error_code, file_function, cplx, percentage_comment] = check_my_code(R
         else
             m_file_ls = get_file_ls(pwd);
         end
-    else 
+    else
         % look through the folder for any m file that we want to check
         if RECURSIVE
             m_file_ls = dir(fullfile(pwd, '**', '*.m'));
@@ -145,11 +158,14 @@ function [error_code, file_function, cplx, percentage_comment] = check_my_code(R
         fclose(FID);
     end
 
+    varargout = {error_code, file_function, cplx, percentage_comment};
+
 end
 
 function percentage = get_percentage_comment(filename)
-    % Now we check how many lines have a "percent" sign in them which could
+    % Check how many lines have a "percent" sign in them which could
     % indicate a comment of any sort
+
     FID = fopen(filename);
 
     line_count = 0;
@@ -174,6 +190,8 @@ function percentage = get_percentage_comment(filename)
 end
 
 function [file_function, cplx] = get_complexity(file_function, cplx, msg, filename)
+    % Loop through the messages and parses them to keep the name of the function and
+    % subfunction and the complexity
 
     % In case this file is empty (i.e MEX file)
     if isempty(msg)
@@ -184,8 +202,6 @@ function [file_function, cplx] = get_complexity(file_function, cplx, msg, filena
 
     else
 
-        % Loop through the messages and parses them to keep the name of the function and
-        % subfunction and the complexity
         for iMsg = 1:numel(msg)
 
             if ~isempty(strfind(msg(iMsg).message, 'McCabe'))
@@ -215,11 +231,13 @@ function [file_function, cplx] = get_complexity(file_function, cplx, msg, filena
 end
 
 function comment_error_code = report_comments(m_file_ls, percentage_comment, COMMENT_THRS)
-    % this reports on the percentage of comments in the file
-    % we check what files have less comments thant the 2 threshold we have set
+    % This reports on the percentage of comments in the file
+    %
+    % We check what files have less comments thant the 2 threshold we have set
     % and we throw a warning or an error depending on the threshold that has
     % been crossed
-    % in either case we list the files incriminated.
+    %
+    % In either case we list the files incriminated.
 
     WARNING_TO_PRINT = 'Not enough comments in the above functions';
     ERROR_TO_PRINT = 'Really not enough comments in the above functions !!!';
@@ -259,10 +277,12 @@ end
 
 function cplx_error_code = report_cplx(cplx, file_function, CPLX_THRS)
     % this reports on the complexity in the files
-    % we check what files have less comments thant the 2 threshold we have set
+    %
+    % We check what files have less comments thant the 2 threshold we have set
     % and we throw a warning or an error depending on the threshold that has
-    % been crossed
-    % in either case we list the files incriminated.
+    % been crossed.
+    %
+    % In either case we list the files incriminated.
 
     WARNING_TO_PRINT = 'Above functions functions are too complex: you might want to refactor';
     ERROR_TO_PRINT = 'Above functions functions are way too complex: refactor them!!!';
@@ -310,17 +330,17 @@ function filename = create_filename(m_file_ls, idx)
     filename = fullfile(m_file_ls(idx).folder, m_file_ls(idx).name);
 end
 
-function [m_file_ls,dir_ls] = get_file_ls(pth)
+function [m_file_ls, dir_ls] = get_file_ls(pth)
     % this returns the list of .m files in designated folder, including the
     % folder name, which is not returned by the 'dir' command in older
     % Matlab versions.
     % if requested, it also returns the list of subfolders, which is useful
     % for recursive folder-digging in older Matlab versions.
-    
-    m_file_ls = dir(fullfile(pth,'*.m'));
-    
+
+    m_file_ls = dir(fullfile(pth, '*.m'));
+
     % adding the 'folder' field which is missing, if there are some files
-    if size(m_file_ls,1)>0
+    if size(m_file_ls, 1) > 0
         m_file_ls(end).folder = [];
         for ifile = 1:numel(m_file_ls)
             m_file_ls(ifile).folder = pth;
@@ -328,31 +348,32 @@ function [m_file_ls,dir_ls] = get_file_ls(pth)
     else
         m_file_ls = [];
     end
-    
+
     % look for subfolders
-    if nargout==2
+    if nargout == 2
         % get list of all 'subfolders'
         tmp_ls = dir(pth);
         dir_ls = char(tmp_ls([tmp_ls.isdir]).name);
         % remove those starting with a '.'
-        dir_ls(strcmp(cellstr(dir_ls(:,1)),'.'),:) = [];
+        dir_ls(strcmp(cellstr(dir_ls(:, 1)), '.'), :) = [];
     end
 end
 
 function  m_file_ls = get_rec_file_ls(pth)
-    % this returns the list of .m files in designated folder as well as 
-    % those from the sub-folders in a recursive way, which is not returned 
+    % this returns the list of .m files in designated folder as well as
+    % those from the sub-folders in a recursive way, which is not returned
     % by the 'dir' command in older Matlab versions.
-    
+
     % start by get the current folder .m files and list of subfolders
-    [m_file_ls,dir_ls] = get_file_ls(pth);
-    n_subfs = size(dir_ls,1);
+    [m_file_ls, dir_ls] = get_file_ls(pth);
+    n_subfs = size(dir_ls, 1);
+    
     % check the subfolders
     if n_subfs
         for isubf = 1:n_subfs
-            pth_subf = fullfile(pth,deblank(dir_ls(isubf,:)));
+            pth_subf = fullfile(pth, deblank(dir_ls(isubf, :)));
             m_file_lsubf = get_rec_file_ls(pth_subf);
-            m_file_ls = [m_file_ls ; m_file_lsubf];
+            m_file_ls = [m_file_ls; m_file_lsubf];
         end
     end
 end
